5c98e6757867908afc6e7b3def21f2fa5c86bcd4
Nabraj Chaudhary <nabrajchaudhary1412@gmail.com>
2021-11-11 13:21:29 +0545

    dark mode for the text

--- src/components/FormElement.jsx
+++ src/components/FormElement.jsx
@@ -1,7 +1,7 @@
 const FormElement = (props) => {
     return (
         <div className="mb-4">
-            <label className="block text-gray-600 text-xl font-normal mb-2">
+            <label className="block text-gray-600 text-xl font-normal mb-2 dark:text-white">
                 {props.formInputTitle}
             </label>
             <input className="bg-gray-100  rounded w-full py-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:ring ring-black  ring-offset-4 focus:ring-opacity-100  transition duration-300 dark:border-white dark:ring-white  dark:bg-white dark:text-black dark:ring-offset-gray-600" type="text" />
