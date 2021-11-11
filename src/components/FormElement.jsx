9b8aff13116e38039c0f9704fdb0c3015d839b65
Nabraj Chaudhary <nabrajchaudhary1412@gmail.com>
2021-11-11 13:15:36 +0545

    Form hover in dark mode

--- src/components/FormElement.jsx
+++ src/components/FormElement.jsx
@@ -4,7 +4,7 @@ const FormElement = (props) => {
             <label className="block text-gray-600 text-xl font-normal mb-2">
                 {props.formInputTitle}
             </label>
-            <input className="bg-gray-100  rounded w-full py-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:ring ring-black  ring-offset-4 focus:ring-opacity-100  transition duration-300" type="text" />
+            <input className="bg-gray-100  rounded w-full py-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline hover:ring ring-black  ring-offset-4 focus:ring-opacity-100  transition duration-300 dark:border-white dark:ring-white  dark:bg-white dark:text-black dark:ring-offset-gray-600" type="text" />
         </div>
     )
 }
